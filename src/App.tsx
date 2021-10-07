2bda3ca6eb5c06e8c0905cf7381ae3b78004fe2a
Vladimir Shits <rav9aa@gmail.com>
2021-10-08 01:00:34 +0300

    lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className= "background">
+        <div className="app-container">
+            <header className="header">
+                <div>
+                    <img className="header__img" src=''/>
+                    <span className= 'header__text'>Social Network</span>
+                </div>
+
+            </header>
+            <nav className= 'nav'>
+                <div className= 'nav__item'>
+                    Profile
+                </div>
+                <div className= 'nav__item'>
+                    Messages
+                </div>
+                <div className= 'nav__item'>
+                    News
+                </div>
+                <div className= 'nav__item'>
+                    Music
+                </div>
+                <div className= 'nav__item'>
+                    Settings
+                </div>
+            </nav>
+            <main className="main" >
+                Main content
+            </main>
+
+
+        </div>
+        </div>
+    );
 }
 
 export default App;
