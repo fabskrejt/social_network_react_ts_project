342477df1d2e7ba8cf7641974ecfd6c1008de9f5
Vladimir Shits <rav9aa@gmail.com>
2021-10-09 18:39:45 +0300

    Added Header and NavBar components

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div>
+                <img className="header__img" src=''/>
+                <span className='header__text'>Social Network</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const NavBar = ()=>{
+return(
+<div>
+    <nav className='nav'>
+        <div className='nav__item'>
+            Profile
+        </div>
+        <div className='nav__item'>
+            Messages
+        </div>
+        <div className='nav__item'>
+            News
+        </div>
+        <div className='nav__item'>
+            Music
+        </div>
+        <div className='nav__item'>
+            Settings
+        </div>
+    </nav>
+</div>
+)
+}
+
+export default NavBar
