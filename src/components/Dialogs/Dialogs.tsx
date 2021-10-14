1c14f6d697987e615e3c0998bff6e835f7afbdc1
Vladimir Shits <rav9aa@gmail.com>
2021-10-14 22:46:29 +0300

    added new component ProfileInfo

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,12 +7,20 @@ type PropsOfDialogItem = {
     name: string
     id: string
 }
-const DialogItem = (props:PropsOfDialogItem) =>{
-
-    return(
+const DialogItem = (props: PropsOfDialogItem) => {
+    return (
         <div className={dStyle.usersItem}>
             <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
         </div>
+    )
+}
+
+type PropsOfMessage = {
+    message: string
+}
+const Message = (props: PropsOfMessage) => {
+    return (
+        <div className={dStyle.dialogsItem}> {props.message}</div>
     )
 }
 
@@ -20,21 +28,18 @@ const Dialogs = () => {
     return (
         <div className={dStyle.dialogPage}>
             <div className={dStyle.users}>
-                <DialogItem name = {'Rusik'} id = {'1'}/>
-                <DialogItem name = {'Toshik'} id = {'2'}/>
-                <DialogItem name = {'Vovchick'} id = {'3'}/>
-                <DialogItem name = {'Dimas'} id = {'4'}/>
-                <DialogItem name = {'Kisli'} id = {'5'}/>
-
-
+                <DialogItem name={'Rusik'} id={'1'}/>
+                <DialogItem name={'Toshik'} id={'2'}/>
+                <DialogItem name={'Vovchick'} id={'3'}/>
+                <DialogItem name={'Dimas'} id={'4'}/>
+                <DialogItem name={'Kisli'} id={'5'}/>
             </div>
             <div className={dStyle.dialogs}>
-                <div className={dStyle.dialogsItem}> Message 1</div>
-                <div className={dStyle.dialogsItem}> Message 2</div>
-                <div className={dStyle.dialogsItem}> Message 3</div>
-                <div className={dStyle.dialogsItem}> Message 4</div>
-                <div className={dStyle.dialogsItem}> Message 5</div>
-
+                <Message message={'Message 1'}/>
+                <Message message={'Message 2'}/>
+                <Message message={'Message 3'}/>
+                <Message message={'Message 4'}/>
+                <Message message={'Message 5'}/>
             </div>
         </div>
     )
