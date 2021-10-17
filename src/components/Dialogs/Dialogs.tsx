6889f1f896dc63110138aa3e3590778eaa1d750c
Vladimir Shits <rav9aa@gmail.com>
2021-10-17 22:37:47 +0300

    apply map for MyPosts component, refactored Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -26,30 +26,33 @@ const Message = (props: PropsOfMessage) => {
 }
 
 const Dialogs = () => {
-    let dialogsData =[
-        {id: 1, name:'Rusik'},
-        {id: 2, name:'Toshik'},
-        {id: 3, name:'Vovchick'},
-        {id: 4, name:'Dimas'},
-        {id: 5, name:'Kisli'},
+    let dialogsData = [
+        {id: 1, name: 'Rusik'},
+        {id: 2, name: 'Toshik'},
+        {id: 3, name: 'Vovchick'},
+        {id: 4, name: 'Dimas'},
+        {id: 5, name: 'Kisli'},
     ]
 
     let messages = [
-        {id : 1, textMessage: 'Message 1'},
-        {id : 2, textMessage: 'Message 2'},
-        {id : 3, textMessage: 'Message 3'},
-        {id : 4, textMessage: 'Message 4'},
-        {id : 5, textMessage: 'Message 5'},
-        {id : 6, textMessage: 'Message 6'},
+        {id: 1, textMessage: 'Message 1'},
+        {id: 2, textMessage: 'Message 2'},
+        {id: 3, textMessage: 'Message 3'},
+        {id: 4, textMessage: 'Message 4'},
+        {id: 5, textMessage: 'Message 5'},
+        {id: 6, textMessage: 'Message 6'},
     ]
+
+    let dialogsElement = dialogsData.map(i => <DialogItem name={i.name} id={i.id}/>)
+    let messagesElement = messages.map(i => <Message id={i.id} message={i.textMessage}/>)
     return (
         <div className={dStyle.dialogPage}>
 
             <div className={dStyle.users}>
-                {dialogsData.map( i => <DialogItem name={i.name} id={i.id}/> )}
+                {dialogsElement}
             </div>
             <div className={dStyle.dialogs}>
-                {messages.map( i =>  <Message id = {i.id} message={i.textMessage}/> )}
+                {messagesElement}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import pStyle from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () =>{
-    return(<div>
-        <section className={pStyle.postsMaker}>
-            <header className='posts-maker__title'>My posts</header>
-            <textarea className={pStyle.input}/>
-            <button className='posts-maker__button'>Send</button>
-        </section>
+const MyPosts = () => {
+    let postsData = [
+        {postText: 'Hello my friends', like: 15},
+        {postText: 'How are you?', like: 5},
+        {postText: 'What are you do?', like: 25},
+    ]
+    let postElement = postsData.map(i => <Post message={i.postText} like={i.like}/>)
+
+    return (<div>
+            <section className={pStyle.postsMaker}>
+                <header className='posts-maker__title'>My posts</header>
+                <textarea className={pStyle.input}/>
+                <button className='posts-maker__button'>Send</button>
+            </section>
             <section className={pStyle.posts}>
-                <Post message ={'Hello my friends'} like={15}/>
-                <Post message ={'How are you?'} like={5}/>
+                {postElement}
             </section>
         </div>
     )
