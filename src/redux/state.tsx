bbd0699126b2478d84af67d5beaf1c82e8f40ad4
Vladimir Shits <rav9aa@gmail.com>
2021-10-22 22:34:34 +0300

    added state.tsx file for data

---
+++ src/redux/state.tsx
@@ -0,0 +1,29 @@
+export let state = {
+    messagesPage: {
+        dialogsData: [
+            {id: 1, name: 'Rusik'},
+            {id: 2, name: 'Toshik'},
+            {id: 3, name: 'Vovchick'},
+            {id: 4, name: 'Dimas'},
+            {id: 5, name: 'Kisli'},
+        ],
+        messages: [
+            {id: 1, textMessage: 'Message 1'},
+            {id: 2, textMessage: 'Message 2'},
+            {id: 3, textMessage: 'Message 3'},
+            {id: 4, textMessage: 'Message 4'},
+            {id: 5, textMessage: 'Message 5'},
+            {id: 6, textMessage: 'Message 6'},
+            {id: 7, textMessage: 'Message 7'},
+        ]
+    },
+    profilePage: {
+        postsData: [
+            {postText: 'Hello my friends', like: 15},
+            {postText: 'How are you?', like: 5},
+            {postText: 'What are you do?', like: 25},
+        ]
+    }
+
+
+}
