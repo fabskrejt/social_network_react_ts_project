9313e5b14b9906cce3f237267cc4d269995668d4
Vladimir Shits <rav9aa@gmail.com>
2021-11-06 22:37:27 +0300

    added store

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {addMessage, addPost, state, StateType, subscribe} from "./redux/state";
+import {store, StateType,/* subscribe*/} from "./redux/state";
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
 
-const renderEntireTree = () => {
+/*const renderEntireTree = () => {*/
     ReactDOM.render(
         <React.StrictMode>
-            <App appState={state} addPost={addPost} addMessage={addMessage}/>
+            <App appState={store.getState()} addPost={store.addPost.bind(store)} addMessage={store.addMessage.bind(store)}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
+/*
 }
 
 renderEntireTree()
 
-subscribe(renderEntireTree)
+subscribe(renderEntireTree)*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -29,11 +29,52 @@ export type  StateType = {
     messagesPage: MessagesPageType
     profilePage: ProfilePageType
 }
-export let renderEntireTree = (state: StateType) => {
+/*export let renderEntireTree = (state: StateType) => {
     console.log('rerender ')
+}*/
+export const store = {
+    _state: {
+        messagesPage: {
+            dialogsData: [
+                {id: v1(), name: 'Rusik'},
+                {id: v1(), name: 'Toshik'},
+                {id: v1(), name: 'Vovchick'},
+                {id: v1(), name: 'Dimas'},
+                {id: v1(), name: 'Kisli'},
+            ],
+            messages: [
+                {id: v1(), textMessage: 'Message 1'},
+                {id: v1(), textMessage: 'Message 2'},
+                {id: v1(), textMessage: 'Message 3'},
+                {id: v1(), textMessage: 'Message 4'},
+                {id: v1(), textMessage: 'Message 5'},
+                {id: v1(), textMessage: 'Message 6'},
+                {id: v1(), textMessage: 'Message 7'},
+            ]
+        },
+        profilePage: {
+            postsData: [
+                {id: v1(), postText: 'Hello my friends', like: 15},
+                {id: v1(), postText: 'How are you?', like: 5},
+                {id: v1(), postText: 'What are you do?', like: 25},
+            ]
+        }
+    },
+    getState() {
+        return this._state
+    },
+    addPost(value: string) {
+        const newPost: PostsDataType = {id: v1(), postText: value, like: 10}
+        this._state.profilePage.postsData.unshift(newPost)
+        /*    renderEntireTree(state)*/
+    },
+    addMessage(value: string) {
+        const newMessage: MessagesType = {id: v1(), textMessage: value}
+        this._state.messagesPage.messages.push(newMessage)
+        /*    renderEntireTree(state)*/
+    }
 }
-
-export let state: StateType = {
+/*export let state: StateType = {
     messagesPage: {
         dialogsData: [
             {id: v1(), name: 'Rusik'},
@@ -59,21 +100,23 @@ export let state: StateType = {
             {id: v1(), postText: 'What are you do?', like: 25},
         ]
     }
-}
+}*/
 
-export const addPost = (value: string) => {
+/*export const addPost = (value: string) => {
     const newPost: PostsDataType = {id: v1(), postText: value, like: 10}
     state.profilePage.postsData.unshift(newPost)
-    renderEntireTree(state)
-}
+/!*    renderEntireTree(state)*!/
+}*/
 
 
-export const addMessage = (value: string) => {
+/*export const addMessage = (value: string) => {
     const newMessage: MessagesType = {id: v1(), textMessage: value}
     state.messagesPage.messages.push(newMessage)
-    renderEntireTree(state)
-}
+    /!*    renderEntireTree(state)*!/
+}*/
 
+/*
 export const subscribe = (observer: () => void) => {
     renderEntireTree = observer
 }
+*/
