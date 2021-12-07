7e361a9987e88a0512fe30a7b1e4f86daf2e2e08
Vladimir Shits <rav9aa@gmail.com>
2021-12-07 22:32:38 +0300

    created get request for users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {v1} from "uuid";
 import {UserType} from "../../redux/users-reducer";
 import style from './Users.module.css'
 
@@ -8,10 +7,18 @@ type UsersPropsType = {
     usersPage: Array<UserType>
     follow: (id: string) => void
 }
+
 export const Users = (props: UsersPropsType) => {
+
     const follow = (id: string) => {
         props.follow(id)
     }
+    const axios = require('axios')
+
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: any) => console.log(response.data.items)
+    )
+
+
     const usersList = props.usersPage.map(i => {
         return (
             <div key={i.id} className={style.user}>
@@ -27,6 +34,7 @@ export const Users = (props: UsersPropsType) => {
             </div>
         )
     })
+
     return (
         //{id: v1(), name: 'Rusik', city: 'Tomsk', education: 'TUSUR', site: 'non', folowed: true},
         <div className={style.usersList}>
