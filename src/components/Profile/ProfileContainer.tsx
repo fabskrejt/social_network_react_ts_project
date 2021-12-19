b2a4e55a0a7a3d23abfb2ad7d2c5515e8dacbd52
Vladimir Shits <rav9aa@gmail.com>
2021-12-19 23:52:39 +0300

    added ProfileContainer component

---
+++ src/components/Profile/ProfileContainer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import pStyle from './Profile.module.css'
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
+
+
+const ProfileContainer = () => {
+    return (
+        <main className={pStyle.main}>
+            <ProfileInfo userName={'Vovchick'} birthday={'31.10.1992'} city={'Rostov-on-Don'} education={'TPU'}
+                         site={'I have not'}/>
+            <MyPostsContainer/>
+        </main>
+    )
+}
+
+export default ProfileContainer
