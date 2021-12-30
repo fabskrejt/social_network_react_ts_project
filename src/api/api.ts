54e919e7463174e3694563c88061a300e9d4ee40
Vladimir Shits <rav9aa@gmail.com>
2021-12-30 11:59:39 +0300

    created instance for axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,15 @@
 import {default as axios} from "axios";
 
+const instance = axios.create(
+    {
+        baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+        withCredentials: true,
+        headers: {
+            'API-KEY': '3938ec03-652e-45e2-8e89-4b9560fc50c6',
+        }
+    }
+)
+
 export const getUser = (pageSize:number, pageNumber:number)=> {
-   return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`,{
-       withCredentials: true
-   })
+   return instance.get(`users?page=${pageNumber}&count=${pageSize}`, )
 }
