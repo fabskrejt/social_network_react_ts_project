b7d8d7ba7eb59a1d540318d1b769c247cecf1dd3
Vladimir Shits <rav9aa@gmail.com>
2022-02-12 22:09:45 +0300

    deleted comments

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,15 +2,12 @@ import React, {ChangeEvent, useState} from "react";
 import dStyle from './Dialogs.module.css'
 import Message from "./Message/Message";
 import DialogItem from "./DialogItem/DialogItem";
-/*import {ActionTypes, MessagesPageType} from "../../redux/state";*/
-import {Button} from "../Button/Button";
 import {MessagesPageType} from "../../redux/dialog-reducer";
-import {Form, Field} from 'react-final-form'
+import {Field, Form} from 'react-final-form'
 
 type DialogsPropsType = {
     dialogsPage: MessagesPageType
     addMessage: (addMessage: string) => void
-    // isAuth: boolean
 }
 
 export const Dialogs = (props: DialogsPropsType) => {
@@ -20,21 +17,13 @@ export const Dialogs = (props: DialogsPropsType) => {
     }
     let dialogsElement = props.dialogsPage.dialogsData.map(i => <DialogItem name={i.name} id={i.id}/>)
     let messagesElement = props.dialogsPage.messages.map(i => <Message id={i.id} message={i.textMessage}/>)
-    const onAddMessage = (value:any) => {
+    const onAddMessage = (value: any) => {
+        debugger
         if (value.message) {
             props.addMessage(value.message)
-            /* props.dispatch(addMessageAC(messageText))*/
             setMessageText('')
         }
-
-        /*        if (messageText) {
-            props.addMessage(messageText)
-            /!* props.dispatch(addMessageAC(messageText))*!/
-            setMessageText('')
-        }*/
     }
-
-    //if (!props.isAuth) return <Redirect to={'/login'}/> //If not authorised, redirect to Login
     return (
         <div className={dStyle.dialogPage}>
             <div className={dStyle.users}>
@@ -44,8 +33,6 @@ export const Dialogs = (props: DialogsPropsType) => {
                 {messagesElement}
                 <DialogsForm messageText={messageText} changeMessageText={changeMessageText}
                              onAddMessage={onAddMessage}/>
-                {/*                <textarea value={messageText} onChange={changeMessageText}/>
-                <Button style={dStyle.button} title={'add'} callBack={onAddMessage}/>*/}
             </div>
         </div>
     )
@@ -54,7 +41,7 @@ export const Dialogs = (props: DialogsPropsType) => {
 type DialogsFormPropsType = {
     messageText: string
     changeMessageText: (e: ChangeEvent<HTMLTextAreaElement>) => void
-    onAddMessage: (value:any) => void
+    onAddMessage: (value: any) => void
 }
 
 export const DialogsForm = (props: DialogsFormPropsType) => {
@@ -62,10 +49,8 @@ export const DialogsForm = (props: DialogsFormPropsType) => {
         <div className={dStyle.dialogs}>
             <Form onSubmit={props.onAddMessage} render={({handleSubmit, values}) => (
                 <form onSubmit={handleSubmit}>
-                    <Field name={'message'} component={'textarea'} />
+                    <Field name={'message'} component={'textarea'}/>
                     <button type="submit">Send</button>
-                    {/*   // <textarea value={props.messageText} onChange={props.changeMessageText}/>
-                    //<Button style={dStyle.button} title={'add'} callBack={props.onAddMessage}/>*/}
                 </form>
             )}/>
 
