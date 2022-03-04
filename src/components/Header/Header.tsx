b5766f3d0ec531b12f1c0f8e49957d86204e6227
Vladimir Shits <rav9aa@gmail.com>
2022-03-04 23:35:16 +0300

    header fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,10 +11,10 @@ type HeaderPropsType = {
 const Header = (props: HeaderPropsType) => {
     return (
         <header className={hStyle.header}>
-            <div>
+            <div className={hStyle.hh}>
                 <img className={hStyle.headerImg}
                      src='https://www.wallpapertip.com/wmimgs/183-1835364_abstract-logo-transparent-background.jpg'/>
-                <span className='header__text'>Social Network</span>
+                <span className={hStyle.headerText}>Social Network</span>
             </div>
             {
                 props.isAuth
