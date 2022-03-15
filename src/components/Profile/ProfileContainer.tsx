dcb315f702cb88a6d9c5fd9bf3c41dec41ce23b3
Vladimir Shits <rav9aa@gmail.com>
2022-03-15 20:29:48 +0300

    refactored code

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -31,8 +31,10 @@ class ProfileContainer extends React.Component<PropsType, InitialProfileStateTyp
     }
 
     componentDidUpdate(prevProps: Readonly<PropsType>, prevState: Readonly<InitialProfileStateType>) {
-        if(this.props.match.params.userId !== prevProps.match.params.userId)
-        this.refreshProfile()
+        if(this.props.match.params.userId !== prevProps.match.params.userId){
+            this.refreshProfile()
+        }
+
     }
 
     render() {
